bee5ae894b3cd35b6414fb06f0de0fb8cc79c4dd
Renzlawr <RSLogan23@gmail.com>
2022-09-06 16:03:18 -0600

    basic file structure

---
+++ src/components/NavBar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../../styles/NavBar.module.scss'
+
+const NavBar = () => {
+  return (
+    <div className={styles.test}>test</div>
+  )
+}
+
+export default NavBar

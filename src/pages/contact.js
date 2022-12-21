591fc0ae1a1a475e6b82a49e3d1ee3fd488e006c
Renzlawr <RSLogan23@gmail.com>
2022-12-20 21:12:14 -0700

    basic form for contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,27 @@ import React from 'react'
 
 const contact = () => {
   return (
-    <div>contact</div>
+    <form>
+      <label for='first-name'>
+        First Name:
+      </label>
+      <input name='first-name' type='text' id='first-name' />
+      <label for='last-name' type='text'>
+        last-name:
+      </label>
+      <input name='last-name' type='text' id='last-name' />
+      <label for='Subject' type='text'>
+        Subject:
+      </label>
+      <input name='subjest' type='text' id='subjest' />
+      <label for='Description' type='text'>
+        Description:
+      </label>
+      <input name='description' type='text' id='description' />
+      <button type="submit">
+        Submit
+      </button>
+    </form>
   )
 }
 

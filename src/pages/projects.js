c8f14db8074c3ee62ed0aa4c83f9fa4c7cc65aaa
Renzlawr <RSLogan23@gmail.com>
2023-01-03 23:35:17 -0700

    projects sass file created

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import styles from '../../styles/projects.module.scss'
 
 const projects = () => {
+  const projectList = [
+    {
+      name: 'test',
+      description: 'this is a bigger test',
+      languages: 'Next.js, Sass, Etc.'
+    },
+    {
+      name: 'test',
+      description: 'this is a bigger test',
+      languages: 'Next.js, Sass, Etc.'
+    },
+    {
+      name: 'test',
+      description: 'this is a bigger test',
+      languages: 'Next.js, Sass, Etc.'
+    }
+  ]
+
+  console.log(projectList.map(project => project.name))
+
   return (
-    <div>projects</div>
+    <div className={styles['projects-container']}>
+      {projectList.map((project, index) => {
+        return (
+          <div key={index} className={styles.project}>
+            <h1>{project.name}</h1>
+            <p>{project.description}</p>
+            <p>{`Coded in: ${project.languages}`}</p>
+          </div>
+        )
+      })}
+    </div>
   )
 }
 

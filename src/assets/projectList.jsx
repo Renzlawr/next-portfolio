05f5011754b6f5d92a872f80658bc2095d45f598
Renzlawr <RSLogan23@gmail.com>
2023-01-04 14:26:44 -0700

    separate file for porject list

---
+++ src/assets/projectList.jsx
@@ -0,0 +1,24 @@
+import profPic from './prof-pic.png'
+
+const projectList = [
+  {
+    name: 'test',
+    image: profPic,
+    description: 'this is a bigger test',
+    languages: 'Next.js, Sass, Etc.'
+  },
+  {
+    name: 'test',
+    image: profPic,
+    description: 'this is a bigger test',
+    languages: 'Next.js, Sass, Etc.'
+  },
+  {
+    name: 'test',
+    image: profPic,
+    description: 'this is a bigger test',
+    languages: 'Next.js, Sass, Etc.'
+  }
+]
+
+export default projectList

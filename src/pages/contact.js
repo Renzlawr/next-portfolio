545bec3dccd6f27ddff8140f494230913637ee0d
Renzlawr <RSLogan23@gmail.com>
2023-01-07 14:57:31 -0700

    conact styling

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,23 +4,29 @@ import styles from '../../styles/contact.module.scss'
 const contact = () => {
   return (
     <form className={styles['contact-container']}>
-      <label for='first-name'>
-        First Name:
-      </label>
-      <input name='first-name' type='text' id='first-name' />
-      <label for='last-name' type='text'>
-        last-name:
-      </label>
-      <input name='last-name' type='text' id='last-name' />
+      <div className={styles['names-container']}>
+        <div className={styles['name-container']}>
+          <label for='first-name'>
+            First Name:
+          </label>
+          <input className={styles['input']} name='first-name' type='text' id='first-name' placeholder='John' />
+        </div>
+        <div className={styles['name-container']}>
+          <label for='last-name' type='text'>
+            Last Name:
+          </label>
+          <input className={styles['input']} name='last-name' type='text' id='last-name' placeholder='Doe' />
+        </div>
+      </div>
       <label for='Subject' type='text'>
         Subject:
       </label>
-      <input name='subjest' type='text' id='subjest' />
+      <input className={styles['input']} name='subjest' type='text' id='subjest' placeholder='Subject'/>
       <label for='Description' type='text'>
         Description:
       </label>
-      <input name='description' type='text' id='description' />
-      <button type="submit">
+      <input className={styles['input-desc']} name='description' type='text-area' id='description'  placeholder='Description'/>
+      <button type="submit" className={styles.submit}>
         Submit
       </button>
     </form>
